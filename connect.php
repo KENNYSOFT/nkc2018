5a6148b31602d8c846b136a42506d7a27c300400
KENNYSOFT <hyeonmin.park@kennysoft.kr>
2018-09-27 17:48:17 +0900

    180927

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
-$connection=mysqli_connect("localhost","nkc","nkc2018");
+$connection=mysqli_connect("localhost","nkc2018","nkc2018");
 if(!$connection)die("Database Connection Failed: ".mysqli_error($connection));
-$select_db=mysqli_select_db($connection,"nkc");
+$select_db=mysqli_select_db($connection,"nkc2018");
 if(!$select_db)die("Database Selection Failed: ".mysqli_error($connection));
 ?>
